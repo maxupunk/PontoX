897a68d04b63bdc0e70bc088c43d225383b62077
Maxuel <maxupunk@yahoo.com.br>
2024-01-04 15:57:53 -0300

    colocado ponto na home

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,7 @@
       <v-app-bar-title>PontoX</v-app-bar-title>
     </v-app-bar>
     <v-navigation-drawer v-model="drawer">
-      <v-list-item link prepend-icon="mdi-home" title="Home" to="/"></v-list-item>
-      <v-list-item link prepend-icon="mdi-eye-check" title="Ponto" to="/ponto"></v-list-item>
+      <v-list-item link prepend-icon="mdi-eye-check" title="Ponto" to="/"></v-list-item>
       <v-list-item link prepend-icon="mdi-school" title="Treinamento" to="/treine"></v-list-item>
       <v-list-item link prepend-icon="mdi-account-group" title="Usuarios" to="/users"></v-list-item>
       <v-list-item link prepend-icon="mdi-information-outline" title="Sobre" to="/about"></v-list-item>
