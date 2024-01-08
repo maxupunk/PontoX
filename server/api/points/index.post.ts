6b5afc2b5e6200760011b19aac68e48e4917e79a
Maxuel <maxupunk@yahoo.com.br>
2024-01-08 19:31:42 -0300

    add post to make point manual

--- server/api/points/index.post.ts
+++ server/api/points/index.post.ts
@@ -1,46 +1,31 @@
 import { points } from "../../../models/points";
 import { db } from "../../sqlite-service";
-import { eq, and, isNull } from "drizzle-orm";
 import { saveUserImage } from '../../../utils/imageUtils';
 
 export default defineEventHandler(async (event) => {
   try {
-    const body = await readBody(event)
+    let body = await readBody(event)
 
-    const date = new Date();
-    const formattedDate = `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)} ${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}:${("0" + date.getSeconds()).slice(-2)}`;
+    if (body.entryImage) {
+      // save file in public/imagens/userId if it is a base64 image
+      body.entryImage = saveUserImage(body.userId, body.entryImage)
+    }
 
-    const pointQuery = db.select()
-      .from(points)
-      .where(
-        and(eq(points.userId, body.userId), isNull(points.departureDate))
-      )
-      .get()
-
-    const nameImage = saveUserImage(body.userId, body.capturedImage)
+    if (body.departureImage) {
+      // save file in public/imagens/userId if it is a base64 image
+      body.departureImage = saveUserImage(body.userId, body.departureImage)
+    }
 
     const data: any = {
       userId: body.userId,
+      entryDate: body.entryDate,
+      entryImage: body.entryImage,
+      departureDate: body.departureDate,
+      departureImage: body.departureImage,
       observation: body.observation,
     }
-
-    if (!pointQuery) {
-      data.entryExpressio = body.expressio
-      data.entryImage = nameImage
-    } else {
-      data.departureExpressio = body.expressio
-      data.departureDate = formattedDate
-      data.departureImage = nameImage
-    }
-
-    if (pointQuery) {
-      return db.update(points).set(data).where(eq(points.id, pointQuery.id)).run()
-    } else {
-      return db.insert(points).values(data).run();
-    }
-
+    return db.insert(points).values(data).run();
   } catch (e: any) {
-    console.log(e)
     throw createError({
       statusCode: 400,
       statusMessage: e.message,
