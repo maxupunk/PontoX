cf6e1a2460a45b37f2f74944163762c83f0efc44
Maxuel <maxupunk@yahoo.com.br>
2024-01-08 19:32:09 -0300

    chage posst to patch

---
+++ server/api/points/index.patch.ts
@@ -0,0 +1,49 @@
+import { points } from "../../../models/points";
+import { db } from "../../sqlite-service";
+import { eq, and, isNull } from "drizzle-orm";
+import { saveUserImage } from '../../../utils/imageUtils';
+
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event)
+
+    const date = new Date();
+    const formattedDate = `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)} ${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+
+    const pointQuery = db.select()
+      .from(points)
+      .where(
+        and(eq(points.userId, body.userId), isNull(points.departureDate))
+      )
+      .get()
+
+    const nameImage = saveUserImage(body.userId, body.capturedImage)
+
+    const data: any = {
+      userId: body.userId,
+      observation: body.observation,
+    }
+
+    if (!pointQuery) {
+      data.entryExpressio = body.expressio
+      data.entryImage = nameImage
+    } else {
+      data.departureExpressio = body.expressio
+      data.departureDate = formattedDate
+      data.departureImage = nameImage
+    }
+
+    if (pointQuery) {
+      return db.update(points).set(data).where(eq(points.id, pointQuery.id)).run()
+    } else {
+      return db.insert(points).values(data).run();
+    }
+
+  } catch (e: any) {
+    console.log(e)
+    throw createError({
+      statusCode: 400,
+      statusMessage: e.message,
+    });
+  }
+});
