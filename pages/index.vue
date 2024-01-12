fef5a1be141bd2849ab6fb67c8d911bb6ac15a25
Maxuel <maxupunk@yahoo.com.br>
2024-01-12 09:37:41 -0300

    fix input lenght

--- pages/index.vue
+++ pages/index.vue
@@ -31,17 +31,17 @@
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12" sm="6" md="6">
+              <v-col cols="12">
                 <v-text-field label="Nome do usuário" v-model="dataUser.user.name" readonly></v-text-field>
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="12" sm="6" md="6">
+              <v-col cols="12">
                 <v-text-field label="Observação" v-model="observation"></v-text-field>
               </v-col>
             </v-row>
             <v-row v-if="dataUser.point">
-              <v-col cols="12" sm="6" md="6">
+              <v-col cols="12">
                 <v-text-field label="Entrada" v-model="dataUser.point.entryDate" readonly></v-text-field>
               </v-col>
             </v-row>
