2c8918f8eb9042155f0584fbc29b3b9b09e7ec85
Maxuel <maxupunk@yahoo.com.br>
2024-01-12 09:35:22 -0300

    add permission file image

--- utils/imageUtils.ts
+++ utils/imageUtils.ts
@@ -7,7 +7,7 @@ export function saveUserImage(userId: Number, capturedImage: string) {
         const dirPath = path.join('public', 'imagens', `${userId}`)
         if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath, { recursive: true })
-            fs.chmodSync(dirPath, '777')
+            fs.chmodSync(dirPath, '774')
         }
 
         // Remove data prefix from the string
@@ -21,7 +21,7 @@ export function saveUserImage(userId: Number, capturedImage: string) {
         const nameImage = date.toISOString().replace(/[:.]/g, '.') + '.jpg'
         const filePath = path.join(dirPath, nameImage);
         fs.writeFileSync(filePath, imageBase64);
-        fs.chmodSync(filePath, '777');
+        fs.chmodSync(filePath, '774');
         return nameImage;
     } else {
         return capturedImage;
