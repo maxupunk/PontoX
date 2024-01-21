ab548c4324c2bb349678b70f102d5126c8841b50
Maxuel <maxupunk@yahoo.com.br>
2024-01-21 19:15:04 -0300

    add image in white list

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,17 @@
 import { users } from "../../models/users";
 import { db } from "../sqlite-service";
 import { eq } from "drizzle-orm";
+import { pathToRegexp } from 'path-to-regexp';
 
 export default defineEventHandler((event) => {
+  const pathWhitelist = ['/api/login', '/', '/api/treine', '/api/imagens/:label/:file'];
+  // Transforma cada caminho da lista de permissões em uma expressão regular
+  const regexWhitelist = pathWhitelist.map(path => pathToRegexp(path));
 
-  const pathWhitelist = ['/api/login', '/', '/api/treine'];
+  // Verifica se a URL do evento corresponde a alguma das expressões regulares
+  const isPathValid = regexWhitelist.some(re => re.test(event.path));
 
-  if (!pathWhitelist.includes(event.path)) {
+  if (!isPathValid) {
     let authorization = event.headers.get('authorization');
     if (authorization) {
       const userQuery = db.select()
