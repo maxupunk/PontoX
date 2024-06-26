c282e1caa630703e62505913e0878c8be084da75
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-06-26 20:34:11 -0300

    prima schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,18 +29,7 @@ async function main() {
               departureExpressio: 'Teste',
               departureImage: 'Teste',
               observation: 'Teste',
-            },
-            {
-              entryDate: '2021-10-11',
-              entryTime: '09:00:00',
-              entryImage: 'Teste',
-              entryExpressio: 'Teste',
-              departureDate: '2021-10-11',
-              departureTime: '10:00:00',
-              departureExpressio: 'Teste',
-              departureImage: 'Teste',
-              observation: 'Teste',
-            },
+            }
           ]
         }
       },
