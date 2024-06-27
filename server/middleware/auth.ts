afe18cdf63383a8fe5ef8ddb245912b1d702fed7
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-06-27 15:19:12 -0300

    migrate to prisma orm

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,9 +1,7 @@
-import { users } from "../../models/users";
-import { db } from "../sqlite-service";
-import { eq } from "drizzle-orm";
+import prisma from "../prisma";
 import { pathToRegexp } from 'path-to-regexp';
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event: any) => {
   const pathWhitelist = ['/api/login', '/', '/login', '/api/treine', '/api/imagens/:label/:file', '/api/relatorios/resumo', '/api/relatorios/:id'];
   // Transforma cada caminho da lista de permissões em uma expressão regular
   const regexWhitelist = pathWhitelist.map(path => pathToRegexp(path));
@@ -14,11 +12,11 @@ export default defineEventHandler((event) => {
   if (!isPathValid) {
     let authorization = event.headers.get('authorization');
     if (authorization) {
-      const userQuery = db.select()
-        .from(users)
-        .where(eq(users.token, authorization))
-        .get()
-
+      const userQuery = await prisma.users.findFirst({
+        where: {
+          token: authorization,
+        },
+      });
       if (!userQuery) {
         throw createError({
           statusCode: 401,
