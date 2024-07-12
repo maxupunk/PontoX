3406b26bbcbfe16a3d43ecef70ffa0581ba32710
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-07-12 11:21:17 -0300

    weekManager initial

---
+++ components/users/weekManager.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-card>
+        <v-toolbar>
+            <v-toolbar-title class="text-uppercase">Horários</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn icon @click="weekStore.saveWeek(userid)">
+                    <v-icon>mdi-content-save</v-icon>
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-card-text>
+            <v-card variant="text" v-for="(hours, dayName) in weekStore.getWeek" :key="dayName">
+                <v-toolbar flat density="compact">
+                    <v-toolbar-title class="text-uppercase">{{ dayName }}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        <v-btn icon @click="weekStore.addHour(dayName)">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+                <v-card dense v-for="(item, i) in hours" :key="i" :color="item.color">
+                    <v-card-text>
+                        <v-row class="d-flex align-center">
+                            <v-col cols="1">
+                                <h3>{{ i + 1 }}</h3>
+                            </v-col>
+                            <v-col cols="5">
+                                <timePick v-model="item.entryTime" label="horário de entrada" />
+                            </v-col>
+                            <v-col cols="5">
+                                <timePick v-model="item.departureTime" :min="item.entryTime" label="horário de saida" />
+                            </v-col>
+                            <v-col cols="1">
+                                <v-btn icon variant="text" @click="weekStore.removeHour(dayName, i)">
+                                    <v-icon>mdi-minus</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
+            </v-card>
+        </v-card-text>
+    </v-card>
+</template>
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import timePick from '~/components/crud/timePick.vue';
+import { useWeekStore } from '~/stores/WeekStore';
+
+const weekStore = useWeekStore()
+defineComponent({
+    name: 'weekManager',
+    components: { timePick },
+})
+
+const props = defineProps({
+    userid: { type: Number, required: true }
+})
+
+onMounted(() => {
+    weekStore.loadWeek(props.userid)
+})
+</script>
+<style lang="css">
+.text-uppercase {
+    text-transform: uppercase;
+}
+</style>
