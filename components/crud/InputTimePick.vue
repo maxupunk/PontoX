2a99f0d95b8904b73ce6e2ae46c6dce87b5039bb
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-07-12 11:37:06 -0300

    fix var name

--- components/crud/InputTimePick.vue
+++ components/crud/InputTimePick.vue
@@ -1,8 +1,8 @@
 <template>
-    <v-text-field density="compact" hide-details v-model="model" :active="menu" :label="label"
+    <v-text-field density="compact" hide-details v-model="model" :active="menu" :label="props.label"
         prepend-inner-icon="mdi-clock-time-four-outline" readonly>
         <v-menu v-model="menu" :close-on-content-click="false" activator="parent" transition="scale-transition">
-            <v-time-picker header-color="primary" v-model="model" :max="max" format="24hr" @update:minute="closeMenu"></v-time-picker>
+            <v-time-picker header-color="primary" v-model="model" :max="props.max" format="24hr" @update:minute="closeMenu"></v-time-picker>
         </v-menu>
     </v-text-field>
 </template>
