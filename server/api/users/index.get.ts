a2a953b9b91b1ed1216ebe162557e21cb6a899f4
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-07-23 18:15:44 -0300

    clear query

--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -2,7 +2,16 @@ import prisma from "~/server/prisma";
 
 export default defineEventHandler(async () => {
   try {
-    const usersResp = await prisma.user.findMany();
+    const usersResp = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        login: true,
+        role: true,
+        status: true,
+      }
+    });
     return { "users": usersResp };
   } catch (e: any) {
     throw createError({
