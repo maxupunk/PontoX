4052eafda1967257732d6bfca4fb94a2ec05fa15
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-09-10 10:20:42 -0300

    add name user in calender

--- app/pages/users/[userid]/calendar.vue
+++ app/pages/users/[userid]/calendar.vue
@@ -1,6 +1,14 @@
 <template>
     <v-container>
         <v-card>
+            <v-toolbar dark prominent>
+                <v-btn icon>
+                    <v-icon>mdi-calendar-account</v-icon>
+                </v-btn>
+                <v-toolbar-title>{{ userStore.user.name }}</v-toolbar-title>
+
+                <v-spacer></v-spacer>
+            </v-toolbar>
             <v-tabs v-model="tab">
                 <v-tab value="week">Semana padrão</v-tab>
                 <v-tab value="month">Gerencia período</v-tab>
@@ -24,13 +32,26 @@
 import { defineComponent, ref } from 'vue'
 import weekManager from '~/components/users/weekManager.vue';
 import periodGenerator from '~/components/users/periodGenerator.vue';
+import { useUserStore } from '~/stores/UserStore';
+
+const userStore = useUserStore()
+
 defineComponent({
     name: 'Calendar',
     components: { weekManager, periodGenerator },
 })
 
+const tab = ref('week')
+
 const { params } = useRoute()
 const userid = Number(params.userid)
-const tab = ref('week')
+
+
+onMounted(async () => {
+    console.log(userid)
+    await userStore.fetchUser(userid).catch((error: any) => {
+        snackbarShow(error, 'error')
+    });
+})
 
 </script>
