e95b401286af0fad764e57051df2c4953e25fe64
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-11-21 15:45:20 -0300

    fix for usage with string

--- app/utils/formatDateBr.ts
+++ app/utils/formatDateBr.ts
@@ -1,3 +1,4 @@
-export function formatDate(date: Date) {
+export function formatDate(isoDate: string): string {
+    const date = new Date(isoDate);
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 }
